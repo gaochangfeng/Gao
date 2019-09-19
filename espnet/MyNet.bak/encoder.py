ce159c3846f5bf96dac358fc4fa75ec62b6d2a4e
gaochangfeng <997463914@qq.com>
2019-09-19 21:37:13 +0800

    Revert "决定进行实验的基础代码"
    This reverts commit b32daa17ab5a3197faa128161883ad7b06541725.
    
    # Conflicts:
    #	espnet/MyNet/attention/win_attention.py
    #	espnet/MyNet/encoder_layer.py
    #	main.py

---
+++ espnet/MyNet.bak/encoder.py
@@ -0,0 +1,149 @@
+import torch
+from espnet.nets.pytorch_backend.transformer.layer_norm import LayerNorm
+from espnet.nets.pytorch_backend.transformer.repeat import repeat
+from espnet.nets.pytorch_backend.transformer.embedding import PositionalEncoding
+from espnet.nets.pytorch_backend.transformer.subsampling import Conv2dSubsampling
+from espnet.nets.pytorch_backend.transformer.encoder_layer import EncoderLayer as EncoderLayerTD
+from espnet.nets.pytorch_backend.transformer.attention import MultiHeadedAttention
+from espnet.nets.pytorch_backend.transformer.positionwise_feed_forward import PositionwiseFeedForward
+from espnet.MyNet.modules.encoder_layer import EncoderLayer as EncoderLayerXL
+
+
+class Encoder(torch.nn.Module):
+    """TransformerXL encoder module,@ref "Transformer-XL_AttentiveLanguageModels BeyondaFixed-LengthContext"
+    :param int idim: input dim
+    :param int time_len: the mems length of the recurrent structure
+    :param int ext_len: the ext_mems length of the recurrent structure
+    :param int attention_dim: dimention of attention
+    :param int attention_heads: the number of heads of multi head attention
+    :param int linear_units: the number of units of position-wise feed forward FixMe not be used
+    :param int num_blocks: the number of decoder blocks
+    :param float dropout_rate: dropout rate
+    :param float attention_dropout_rate: dropout rate in attention
+    :param float positional_dropout_rate: dropout rate after adding positional encoding FixMe not be used
+    :param str or torch.nn.Module input_layer: input layer type
+    :param class pos_enc_class: PositionalEncoding or ScaledPositionalEncoding
+    :param bool normalize_before: whether to use layer_norm before the first block
+    :param bool concat_after: whether to concat attention layer's input and output FixMe not be used
+        if True, additional linear will be applied. i.e. x -> x + linear(concat(x, att(x)))
+        if False, no additional linear will be applied. i.e. x -> x + att(x)
+    """
+
+    def __init__(self, idim, time_len=8,mem_len=0, ext_len=0,future_len=0,
+                 attention_type="memory",
+                 attention_dim=256,
+                 attention_heads=4,
+                 linear_units=2048,
+                 num_blocks=6,
+                 dropout_rate=0.1,
+                 positional_dropout_rate=0.1,
+                 attention_dropout_rate=0.0,
+                 input_layer="conv2d",
+                 pos_enc_class=PositionalEncoding,
+                 normalize_before=True,
+                 concat_after=False):
+        super(Encoder, self).__init__()
+        self.idim = idim
+        self.time_len = time_len
+        self.future_len = future_len
+        self.attention_dim = attention_dim
+        self.attention_heads = attention_heads
+        self.linear_units = linear_units
+        self.dropout_rate = dropout_rate
+        self.input_layer = input_layer
+        self.normalize_before = normalize_before
+        self.concat_after = concat_after
+        self.attention_type = attention_type
+        self.positional_dropout_rate = positional_dropout_rate
+        self.pos_enc_class = pos_enc_class
+        self._generateInputLayer()
+        if attention_type == "memory":
+            self.encoders = repeat(
+                num_blocks,
+                lambda: EncoderLayerXL(
+                    n_head=attention_heads,
+                    d_model=attention_dim,
+                    d_head=attention_dim // attention_heads,
+                    ext_len=ext_len,
+                    mem_len=mem_len,
+                    future_len=future_len,
+                    dropout=dropout_rate,
+                    dropatt=attention_dropout_rate,
+                    pre_lnorm=normalize_before,
+                    pos_ff=PositionwiseFeedForward(attention_dim, linear_units, dropout_rate)
+                )
+            )
+        elif attention_type == "traditional":
+            self.encoders = repeat(
+                num_blocks,
+                lambda: EncoderLayerTD(
+                    attention_dim,
+                    MultiHeadedAttention(attention_heads, attention_dim, attention_dropout_rate),
+                    PositionwiseFeedForward(attention_dim, linear_units, dropout_rate),
+                    dropout_rate,
+                    normalize_before,
+                    concat_after
+                )
+            )
+        else:
+            ValueError("only memory or traditional can be used")
+        if self.normalize_before:
+            self.after_norm = LayerNorm(attention_dim)
+
+    def forward(self, xs, masks=None):
+        """Embed positions in tensor
+
+        :param torch.Tensor xs: input tensor，(batch,time,dim)
+        :param torch.Tensor masks: (batch,1,time),1 means data,zero means padding
+        :return: position embedded tensor and mask
+        :rtype Tuple[torch.Tensor, torch.Tensor]:
+        """
+        if isinstance(self.embed, Conv2dSubsampling):
+            xs, masks = self.embed(xs, masks)
+        else:
+            xs = self.embed(xs)
+        if self.time_len <= 0:
+            xs, masks = self.encoders(xs, masks)
+        else:
+            chunks = torch.split(xs,self.time_len,dim=1)
+            if masks is not None:
+                chunk_mask = torch.split(masks,self.time_len,dim=2)
+            else:
+                chunk_mask = None
+            chunks_ans = []
+            for i in range(len(chunks)):
+                if chunk_mask is not None:
+                    xss,_ = self.encoders(chunks[i],chunk_mask[i])
+                else:
+                    xss, _ = self.encoders(chunks[i], None)
+                chunks_ans.append(xss)
+            xs = torch.cat(chunks_ans,dim=1)
+            for layer in self.encoders._modules.values():
+                if isinstance(layer,EncoderLayerXL):
+                    layer.mems=None
+        if self.normalize_before:
+            xs = self.after_norm(xs)
+        return xs, masks
+
+    def _generateInputLayer(self):
+        if self.input_layer == "linear":
+            self.embed = torch.nn.Sequential(
+                torch.nn.Linear(self.idim, self.attention_dim),
+                torch.nn.LayerNorm(self.attention_dim),
+                torch.nn.Dropout(self.dropout_rate),
+                torch.nn.ReLU(),
+            )
+            if self.attention_type == "traditional":
+                self.embed.add_module(name="pos_enc", module=self.pos_enc_class(self.attention_dim, self.positional_dropout_rate))
+        elif self.input_layer == "conv2d":
+            self.embed = Conv2dSubsampling(self.idim, self.attention_dim, self.dropout_rate)
+        elif self.input_layer == "embed":
+            self.embed = torch.nn.Sequential(
+                torch.nn.Embedding(self.idim, self.attention_dim),
+            )
+            if self.attention_type == "traditional":
+                self.embed.add_module("pos_enc", self.pos_enc_class(self.attention_dim, self.positional_dropout_rate))
+        elif isinstance(self.input_layer, torch.nn.Module):
+            self.embed = self.input_layer
+        else:
+            raise ValueError("unknown input_layer: " + self.input_layer)
---
+++ espnet/MyNet.bak/repos_attention.py
@@ -0,0 +1,335 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+FILL_INF = 9.9e20
+
+
+class RelPositionalEmbedding(nn.Module):
+    def __init__(self, demb):
+        super(RelPositionalEmbedding, self).__init__()
+
+        self.demb = demb
+
+        inv_freq = 1 / (10000 ** (torch.arange(0.0, demb, 2.0) / demb))
+        self.register_buffer('inv_freq', inv_freq)
+
+    def forward(self, pos_seq, bsz=None):
+        sinusoid_inp = torch.ger(pos_seq, self.inv_freq)
+        pos_emb = torch.cat([sinusoid_inp.sin(), sinusoid_inp.cos()], dim=-1)
+
+        if bsz is not None:
+            return pos_emb[:, None, :].expand(-1, bsz, -1)
+        else:
+            return pos_emb[:, None, :]
+
+
+# class PositionwiseFF(nn.Module):
+#     def __init__(self, d_model, d_inner, dropout, pre_lnorm=False):
+#         super(PositionwiseFF, self).__init__()
+#
+#         self.d_model = d_model
+#         self.d_inner = d_inner
+#         self.dropout = dropout
+#         self.CoreNet = nn.Sequential(
+#             nn.Linear(d_model, d_inner), nn.ReLU(inplace=True),
+#             nn.Dropout(dropout),
+#             nn.Linear(d_inner, d_model),
+#             nn.Dropout(dropout),
+#         )
+#
+#         self.layer_norm = nn.LayerNorm(d_model)
+#
+#         self.pre_lnorm = pre_lnorm
+#
+#     def forward(self, inp):
+#         if self.pre_lnorm:
+#             ##### layer normalization + positionwise feed-forward
+#             core_out = self.CoreNet(self.layer_norm(inp))
+#
+#             ##### residual connection
+#             output = core_out + inp
+#         else:
+#             ##### positionwise feed-forward
+#             core_out = self.CoreNet(inp)
+#
+#             ##### residual connection + layer normalization
+#             output = self.layer_norm(inp + core_out)
+#
+#         return output
+
+
+class RelMultiHeadAttn(nn.Module):
+    def __init__(self, n_head, d_model, d_head, dropout, dropatt=0,
+                 tgt_len=None, ext_len=None, mem_len=None, pre_lnorm=False):
+        super(RelMultiHeadAttn, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.d_head = d_head
+        self.dropout = dropout
+
+        self.qkv_net = nn.Linear(d_model, 3 * n_head * d_head, bias=False)
+
+        self.drop = nn.Dropout(dropout)
+        self.dropatt = nn.Dropout(dropatt)
+        self.o_net = nn.Linear(n_head * d_head, d_model, bias=False)
+
+        self.layer_norm = nn.LayerNorm(d_model)
+
+        self.scale = 1 / (d_head ** 0.5)
+
+        self.pre_lnorm = pre_lnorm
+
+    def _parallelogram_mask(self, h, w, left=False):
+        mask = torch.ones((h, w)).byte()
+        m = min(h, w)
+        mask[:m, :m] = torch.triu(mask[:m, :m])
+        mask[-m:, -m:] = torch.tril(mask[-m:, -m:])
+
+        if left:
+            return mask
+        else:
+            return mask.flip(0)
+
+    def _shift(self, x, qlen, klen, mask, left=False):
+        if qlen > 1:
+            zero_pad = torch.zeros((x.size(0), qlen - 1, x.size(2), x.size(3)),
+                                   device=x.device, dtype=x.dtype)
+        else:
+            zero_pad = torch.zeros(0, device=x.device, dtype=x.dtype)
+
+        if left:
+            mask = mask.flip(1)
+            x_padded = torch.cat([zero_pad, x], dim=1).expand(qlen, -1, -1, -1)
+        else:
+            x_padded = torch.cat([x, zero_pad], dim=1).expand(qlen, -1, -1, -1)
+
+        x = x_padded.masked_select(mask[:, :, None, None]) \
+            .view(qlen, klen, x.size(2), x.size(3))
+
+        return x
+
+    def _rel_shift(self, x, zero_triu=False):
+        zero_pad = torch.zeros((x.size(0), 1, *x.size()[2:]),
+                               device=x.device, dtype=x.dtype)
+        x_padded = torch.cat([zero_pad, x], dim=1)
+
+        x_padded = x_padded.view(x.size(1) + 1, x.size(0), *x.size()[2:])
+
+        x = x_padded[1:].view_as(x)
+
+        if zero_triu:
+            ones = torch.ones((x.size(0), x.size(1)))
+            x = x * torch.tril(ones, x.size(1) - x.size(0))[:, :, None, None]
+
+        return x
+
+    def forward(self, w, r, attn_mask=None, mems=None):
+        raise NotImplementedError
+
+
+class RelLearnableMultiHeadAttn(RelMultiHeadAttn):
+    def __init__(self, *args, **kwargs):
+        super(RelLearnableMultiHeadAttn, self).__init__(*args, **kwargs)
+
+    def forward(self, w, r_emb, r_w_bias, r_bias, attn_mask=None, mems=None):
+        # r_emb: [klen, n_head, d_head], used for term B
+        # r_w_bias: [n_head, d_head], used for term C
+        # r_bias: [klen, n_head], used for term D
+
+        qlen, bsz = w.size(0), w.size(1)
+        if mems is not None:
+            cat = torch.cat([mems, w], 0)
+            if self.pre_lnorm:
+                w_heads = self.qkv_net(self.layer_norm(cat))
+            else:
+                w_heads = self.qkv_net(cat)
+            w_head_q, w_head_k, w_head_v = torch.chunk(w_heads, 3, dim=-1)
+
+            w_head_q = w_head_q[-qlen:]
+        else:
+            if self.pre_lnorm:
+                w_heads = self.qkv_net(self.layer_norm(w))
+            else:
+                w_heads = self.qkv_net(w)
+            w_head_q, w_head_k, w_head_v = torch.chunk(w_heads, 3, dim=-1)
+
+        klen = w_head_k.size(0)
+
+        w_head_q = w_head_q.view(qlen, bsz, self.n_head, self.d_head)
+        w_head_k = w_head_k.view(klen, bsz, self.n_head, self.d_head)
+        w_head_v = w_head_v.view(klen, bsz, self.n_head, self.d_head)
+        # print("r_emb origen",r_emb.size())
+        if klen > r_emb.size(0):
+            r_emb_pad = r_emb[0:1].expand(klen - r_emb.size(0), -1, -1)
+            r_emb = torch.cat([r_emb_pad, r_emb], 0)
+            r_bias_pad = r_bias[0:1].expand(klen - r_bias.size(0), -1)
+            r_bias = torch.cat([r_bias_pad, r_bias], 0)
+        else:
+            r_emb = r_emb[-klen:]
+            r_bias = r_bias[-klen:]
+
+        # compute attention score
+        rw_head_q = w_head_q + r_w_bias[None]  # qlen x bsz x n_head x d_head
+        AC = torch.einsum('ibnd,jbnd->ijbn', [rw_head_q, w_head_k])  # qlen x klen x bsz x n_head
+        # print('w_head_q',w_head_q.size())
+        # print("r_emb",r_emb.size())
+        B_ = torch.einsum('ibnd,jnd->ijbn', [w_head_q, r_emb])  # qlen x klen x bsz x n_head
+        D_ = r_bias[None, :, None]  # 1    x klen x 1   x n_head
+        BD = self._rel_shift(B_ + D_)
+
+        # [qlen x klen x bsz x n_head]
+        attn_score = AC + BD
+        attn_score.mul_(self.scale)
+
+        # compute attention probability
+        if attn_mask is not None and attn_mask.any().item():
+            if attn_mask.dim() == 2:
+                attn_score.masked_fill_(attn_mask[None, :, :, None], -FILL_INF)
+            elif attn_mask.dim() == 3:
+                attn_score.masked_fill_(attn_mask[:, :, :, None], -FILL_INF)
+
+        # [qlen x klen x bsz x n_head]
+        attn_prob = F.softmax(attn_score, dim=1)
+        attn_prob = self.dropatt(attn_prob)
+
+        # compute attention vector
+        attn_vec = torch.einsum('ijbn,jbnd->ibnd', [attn_prob, w_head_v])
+
+        # [qlen x bsz x n_head x d_head]
+        attn_vec = attn_vec.contiguous().view(
+            attn_vec.size(0), attn_vec.size(1), self.n_head * self.d_head)
+
+        # linear projection
+        attn_out = self.o_net(attn_vec)
+        attn_out = self.drop(attn_out)
+
+        if self.pre_lnorm:
+            # residual connection
+            output = w + attn_out
+        else:
+            # residual connection + layer normalization
+            output = self.layer_norm(w + attn_out)
+
+        return output
+
+
+class RelPartialLearnableMultiHeadAttn(RelMultiHeadAttn):
+    def __init__(self, *args, **kwargs):
+        super(RelPartialLearnableMultiHeadAttn, self).__init__(*args, **kwargs)
+
+        self.r_net = nn.Linear(self.d_model, self.n_head * self.d_head, bias=False)
+
+    def _maskscore(self, att_score, mask):
+        if mask is None:
+            return att_score
+        m_len = att_score.size(1) - mask.size(1)
+        if (m_len > 0):
+            m_mask = torch.zeros(mask.size(0), m_len).byte().to(mask.device)
+            mask = torch.cat([m_mask,mask], dim=1)
+        att_score= att_score.transpose(1, 3)
+        for i in range(mask.size(0)):
+            att_score[:, :, i, :]=att_score[:,:,i,:].masked_fill(mask[i],-FILL_INF)
+        att_score = att_score.transpose(1, 3)
+
+        # att_score = att_score.transpose(2, 3).transpose(0, 2)
+        # print(att_score.size())
+        # mask = mask.t()
+        # for i in range(att_score.size(0)):
+        #     att_score[i] = att_score[i].masked_fill(mask, -FILL_INF)
+        # #att_score = att_score.masked_fill(mask, -FILL_INF)
+        # att_score = att_score.transpose(0, 2).transpose(3, 2)
+        return att_score
+
+    def forward(self, w, r, r_w_bias, r_r_bias, attn_mask=None, mems=None):
+        qlen, rlen, bsz = w.size(0), r.size(0), w.size(1)
+
+        if mems is not None:
+            cat = torch.cat([mems, w], 0)
+            if self.pre_lnorm:
+                w_heads = self.qkv_net(self.layer_norm(cat))
+            else:
+                w_heads = self.qkv_net(cat)
+            r_head_k = self.r_net(r)
+
+            w_head_q, w_head_k, w_head_v = torch.chunk(w_heads, 3, dim=-1)
+            w_head_q = w_head_q[-qlen:]
+        else:
+            if self.pre_lnorm:
+                w_heads = self.qkv_net(self.layer_norm(w))
+            else:
+                w_heads = self.qkv_net(w)
+            r_head_k = self.r_net(r)
+
+            w_head_q, w_head_k, w_head_v = torch.chunk(w_heads, 3, dim=-1)
+
+        klen = w_head_k.size(0)
+
+        w_head_q = w_head_q.view(qlen, bsz, self.n_head, self.d_head)  # qlen x bsz x n_head x d_head
+        w_head_k = w_head_k.view(klen, bsz, self.n_head, self.d_head)  # klen x bsz x n_head x d_head
+        w_head_v = w_head_v.view(klen, bsz, self.n_head, self.d_head)  # klen x bsz x n_head x d_head
+
+        r_head_k = r_head_k.view(rlen, self.n_head, self.d_head)  # rlen x n_head x d_head
+
+        #### compute attention score
+        rw_head_q = w_head_q + r_w_bias  # qlen x bsz x n_head x d_head
+        AC = torch.einsum('ibnd,jbnd->ijbn', [rw_head_q, w_head_k])  # qlen x klen x bsz x n_head
+
+        rr_head_q = w_head_q + r_r_bias
+        BD = torch.einsum('ibnd,jnd->ijbn', [rr_head_q, r_head_k])  # qlen x klen x bsz x n_head
+        BD = self._rel_shift(BD)
+
+        # [qlen x klen x bsz x n_head]
+        attn_score = AC + BD
+        attn_score.mul_(self.scale)
+        # print(attn_score.size())
+        #### compute attention probability
+        # if attn_mask is not None and attn_mask.any().item():
+        #     if attn_mask.dim() == 2:
+        #         attn_score = attn_score.float().masked_fill(
+        #             attn_mask[None, :, :, None], -float('inf')).type_as(attn_score)
+        #     elif attn_mask.dim() == 3:
+        #         attn_score = attn_score.float().masked_fill(
+        #             attn_mask[:, :, :, None], -float('inf')).type_as(attn_score)
+        attn_score = self._maskscore(attn_score, attn_mask)
+        # [qlen x klen x bsz x n_head]
+        attn_prob = F.softmax(attn_score, dim=1)
+        attn_prob = self.dropatt(attn_prob)
+        #### compute attention vector
+        attn_vec = torch.einsum('ijbn,jbnd->ibnd', [attn_prob, w_head_v])
+        # [qlen x bsz x n_head x d_head]
+        attn_vec = attn_vec.contiguous().view(
+            attn_vec.size(0), attn_vec.size(1), self.n_head * self.d_head)
+
+        ##### linear projection
+        attn_out = self.o_net(attn_vec)
+        attn_out = self.drop(attn_out)
+        if self.pre_lnorm:
+            ##### residual connection
+            output = w + attn_out
+        else:
+            ##### residual connection + layer normalization
+            output = self.layer_norm(w + attn_out)
+
+        return output
+
+
+class RelPartialLearnableDecoderLayer(nn.Module):
+    def __init__(self, n_head, d_model, d_head, dropout, pos_ff,
+                 **kwargs):
+        super(RelPartialLearnableDecoderLayer, self).__init__()
+
+        self.dec_attn = RelPartialLearnableMultiHeadAttn(n_head, d_model,
+                                                         d_head, dropout, **kwargs)
+        # self.pos_ff = PositionwiseFF(d_model, d_inner, dropout,
+        #                              pre_lnorm=kwargs.get('pre_lnorm'))
+        self.pos_ff = pos_ff
+
+    def forward(self, dec_inp, r, r_w_bias, r_r_bias, dec_attn_mask=None, mems=None):
+        output = self.dec_attn(dec_inp, r, r_w_bias, r_r_bias,
+                               attn_mask=dec_attn_mask,
+                               mems=mems)
+        output = self.pos_ff(output)
+
+        return output
